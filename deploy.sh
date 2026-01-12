6930feac7941fade9d1b63a9300b3c12fc596cda
kuwa <h.kuwabara1103@gmail.com>
2026-01-12 12:30:35 +0900

    デプロイコード更新

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+
+# .env.localから環境変数を読み込む
+set -a
+source cleaning_report_app/.env.local
+set +a
+
 cd cleaning_report_app
-fvm flutter build web --release --base-href "/cleaning-report/"
+fvm flutter build web --release \
+  --base-href "/cleaning-report/" \
+  --dart-define=SUPABASE_URL="$SUPABASE_URL" \
+  --dart-define=SUPABASE_ANON_KEY="$SUPABASE_ANON_KEY"
 cd ..
 rm -rf docs/*
 cp -r cleaning_report_app/build/web/* docs/
